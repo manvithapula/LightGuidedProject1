c130c7ee81dc7b2b29d3c081e119b1a7db22ff7b
manvithapula <manvithapula@gmail.com>
2024-09-26 18:59:21 +0530

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by admin64 on 26/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
